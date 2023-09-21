69b757e37ceb488c720bd758acebd334f62ecaef
Tony Germaneri <Tony.Germaneri@gmail.com>
2023-09-21 01:12:38 -0700

    improved vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,42 @@
-import { fileURLToPath, URL } from "node:url";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import { resolve } from 'path';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 8080,
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.esm-bundler.js',
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig((mode) => {
+  return {
+    server: {
+      port: 8080,
     },
-  },
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      vuetify(),
+    ],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        './runtimeConfig': './runtimeConfig.browser',
+      },
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        all: true,
+        provider: 'c8',
+        exclude: [
+          '**/node_modules/**', // Exclude files in the 'node_modules' directory
+          '**/coverage/**', // Exclude files in the 'coverage' directory
+          '**/__mocks__/**', // Exclude files in mocks directory
+          '**/__tests__/**', // Exclude files in mocks directory
+          '**/*.config.{js,jsx,ts,tsx}', // Exclude config files
+          '**/dist/**', // Exclude dist files
+          '**/src/plugins/**', // Exclude vendor plugin files
+          '.eslintrc.cjs', // eslint files
+        ],
+      },
+    },
+  };
 });
